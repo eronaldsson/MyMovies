1e887139ca8beded738bdac242fd7079018fe310
Arnold Kristiansen <k_arnold95@hotmail.com>
2022-09-22 16:24:25 +0100

    Created search controller and vidw to fetch all moview (simple)

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMovies.Models;
+
+namespace MyMovies.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        private IMyMoviesRepository repository;
+
+        public SearchController(IMyMoviesRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult GetAll() 
+        {
+            return View(repository.Movies);
+        }
+    }
+
+}
