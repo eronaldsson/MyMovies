fe6e70f3ad60c38ea64b8fd45ae2b88e399c2aca
Ingvard Emil <ingvard.ron20@student.setur.fo>
2022-10-03 13:22:48 +0100

    Added new migration

---
+++ Migrations/20221003122216_ChangedTables.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyMovies.Migrations
+{
+    public partial class ChangedTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Creator",
+                table: "WatchLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "WatchLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Rating",
+                table: "WatchListMovies",
+                type: "decimal(3,1)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Watched",
+                table: "WatchListMovies",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Creator",
+                table: "WatchLists");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "WatchLists");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "WatchListMovies");
+
+            migrationBuilder.DropColumn(
+                name: "Watched",
+                table: "WatchListMovies");
+        }
+    }
+}
