5b8f19dde7d31e1b960f95e0e3f8c11f4b931ba2
Ingvard Emil <ingvard.ron20@student.setur.fo>
2022-10-03 11:25:12 +0100

    Added new mirgation

--- Migrations/20221003100608_CreatedCompositeKey.Designer.cs
+++ Migrations/20221003100608_CreatedCompositeKey.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyMovies.Models;
 
@@ -10,9 +11,10 @@ using MyMovies.Models;
 namespace MyMovies.Migrations
 {
     [DbContext(typeof(MyMoviesDbContext))]
-    partial class MyMoviesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221003100608_CreatedCompositeKey")]
+    partial class CreatedCompositeKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20221003100608_CreatedCompositeKey.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyMovies.Migrations
+{
+    public partial class CreatedCompositeKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WatchListMovies",
+                table: "WatchListMovies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WatchListMovies_WatchListId",
+                table: "WatchListMovies");
+
+            migrationBuilder.DropColumn(
+                name: "WatchListMoviesId",
+                table: "WatchListMovies");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WatchListMovies",
+                table: "WatchListMovies",
+                columns: new[] { "WatchListId", "MovieId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WatchListMovies",
+                table: "WatchListMovies");
+
+            migrationBuilder.AddColumn<long>(
+                name: "WatchListMoviesId",
+                table: "WatchListMovies",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WatchListMovies",
+                table: "WatchListMovies",
+                column: "WatchListMoviesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WatchListMovies_WatchListId",
+                table: "WatchListMovies",
+                column: "WatchListId");
+        }
+    }
+}
