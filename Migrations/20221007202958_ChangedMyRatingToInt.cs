2fe9bd6bc80dc03d2b0244365a90b1237fb5861d
Ingvard Emil <ingvard.ron20@student.setur.fo>
2022-10-07 21:30:50 +0100

    Added new migration

---
+++ Migrations/20221007202958_ChangedMyRatingToInt.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyMovies.Migrations
+{
+    public partial class ChangedMyRatingToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "WatchListMovies",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(3,1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rating",
+                table: "WatchListMovies",
+                type: "decimal(3,1)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
